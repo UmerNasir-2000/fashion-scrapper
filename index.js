4d069656c80577b10b47c996c867386d54b329f5
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-06-01 21:48:47 +0500

    feat: improve web scraping functionality for fetching articles

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const puppeteer = require("puppeteer");
 const URL = `https://asimjofa.com/collections/unstitched`;
 
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--disable-features=SameSiteByDefaultCookies"],
+    defaultViewport: null,
+  });
+
   const page = await browser.newPage();
 
   await page.goto(URL);
@@ -12,8 +17,10 @@ async function run() {
   const links = await getProductLinks(page);
 
   for (let link of links) {
+    console.log(`Fetching article from ${link}`);
     const article = await extractArticle(page, link);
     console.log(article);
+    console.log(`Fetched...`);
   }
 
   await browser.close();
@@ -33,32 +40,18 @@ async function getProductLinks(page) {
 
 async function extractArticle(page, link) {
   await page.goto(link);
-
   const images = await page.$$eval("img[data-master]", (elements) => {
-    return elements.map((el) => {
-      let srcset = el.srcset;
-      let urls = srcset
-        .split(",")
-        .map((url) => {
-          url = url.trim();
-          let jpgIndex = url.indexOf(".jpg");
-          let jpegIndex = url.indexOf(".jpeg");
-          if (jpgIndex !== -1) {
-            url = url.substring(0, jpgIndex + 4);
-          } else if (jpegIndex !== -1) {
-            url = url.substring(0, jpegIndex + 5);
-          }
-          if (url.length === 0) {
-            return null;
-          }
-          return "https:" + url;
-        })
-        .filter(Boolean); // filter out null values
-      return urls.filter(Boolean);
-    });
+    return elements.reduce((acc, el) => {
+      const imgSources = el.srcset
+        .split(", ")
+        .filter(Boolean)
+        .map((src) => `https:${src.split(" ")[0]}`);
+      acc.push(...imgSources);
+      return acc;
+    }, []);
   });
 
-  return images.filter(Boolean);
+  return images;
 }
 
 run().catch(console.error);
