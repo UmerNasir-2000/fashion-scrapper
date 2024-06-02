62ee95df8ddada25d5b347f554d2783851fab527
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-06-02 12:55:14 +0500

    feat: extract meta about each article

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ async function run() {
 
   const links = await getProductLinks(page);
 
-  const articles = await Promise.all(
-    links.map((link) => extractArticle(page, link))
-  );
+  const articles = [];
+  for (let link of links.slice(0, 2)) {
+    console.log(`Extracting article from ${link}`);
+    const article = await extractArticle(page, link);
+    articles.push(article);
+  }
 
   await fs.writeFile("articles.json", JSON.stringify(articles, null, 2));
 
@@ -52,20 +55,29 @@ async function extractArticle(page, link) {
   });
 
   const title = await page.$eval(".t4s-product__title", (el) => el.textContent);
-  const price = await page.$eval(
-    ".t4s-product-price span.money",
-    (el) => el.textContent
+
+  const price = await page.$eval(".t4s-product-price span.money", (el) =>
+    Number(el.textContent.replace("Rs. ", "").replace(",", ""))
   );
-  const description = await page.$eval(
-    ".panel p:nth-child(2)",
-    (el) => el.textContent
+
+  const description = await page.$eval(".panel p:nth-child(2)", (el) =>
+    el.textContent.trim()
   );
 
   const designDetails = await page.$eval(".panel ul", (el) =>
-    Array.from(el.querySelectorAll("li"), (li) => li.textContent)
+    Array.from(el.querySelectorAll("li"), (li) => li.textContent.trim())
   );
 
-  return { title, price, description, designDetails, images };
+  const meta = await page.$$eval(".skus", (elements) =>
+    elements.map((el) => {
+      let [key, ...value] = el.textContent.trim().split(":");
+      key = key.trim() === "By Pcs" ? "pieces" : key.trim();
+      key = key.charAt(0).toLowerCase() + key.slice(1);
+      return { [key]: value.join(":").trim() };
+    })
+  );
+
+  return { title, price, description, designDetails, images, meta };
 }
 
 run().catch(console.error);
